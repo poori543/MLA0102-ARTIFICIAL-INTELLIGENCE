883fec8debed244c01cffe77f763daeee68e5929
poori543 <priyapoorni2006@gmail.com>
2026-01-27 16:22:17 +0530

    min-max using heuristic values (out put).py

---
+++ min-max using heuristic values (out put).py
@@ -0,0 +1,6 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+====== RESTART: C:/Users/priya/Downloads/min-max using heuristic values.py =====
+Enter leaf values: 2 3 5 9 0 1 7 5
+Optimal value: 3
