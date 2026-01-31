8627c850d59de2a606df603c72b0b7c4f77263d9
poori543 <priyapoorni2006@gmail.com>
2026-01-31 10:29:10 +0530

    decision tree 2 output.py

---
+++ decision tree 2 output.py
@@ -0,0 +1,13 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: C:/Users/priya/Downloads/decision tree 2.py =============
+Entropy of PlayTennis: 0.94
+
+Information Gain for each attribute:
+Outlook = 0.247
+Temperature = 0.029
+Humidity = 0.152
+Wind = 0.048
+
+Best Attribute (Root Node): Outlook
