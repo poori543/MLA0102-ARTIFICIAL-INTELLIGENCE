34989c793930b7db8618aeae1fe9a004636aea0a
poori543 <priyapoorni2006@gmail.com>
2026-01-31 10:23:14 +0530

    decision tree 1 output.py

---
+++ decision tree 1 output.py
@@ -0,0 +1,12 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: C:/Users/priya/Downloads/decision tree 1.py =============
+Entropy of Class: 0.9709505944546686
+
+Information Gain:
+A1 = 0.6099865470109874
+A2 = 0.12451124978365313
+A3 = 0.4199730940219749
+
+Best Attribute for Root Node: A1
