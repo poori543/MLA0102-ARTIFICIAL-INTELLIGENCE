c90120dea8c137150902ba1662960096b982354e
poori543 <priyapoorni2006@gmail.com>
2026-02-02 08:54:09 +0530

    water jug problem (out put).py

---
+++ water jug problem (out put).py
@@ -0,0 +1,12 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/priya/Downloads/water jug problem.py ============
+Steps to reach target:
+(0, 0)
+(5, 0)
+(2, 3)
+(2, 0)
+(0, 2)
+(5, 2)
+(4, 3)
