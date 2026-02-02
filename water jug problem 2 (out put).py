8847163471ab01c111d94f90b1b6308812551681
poori543 <priyapoorni2006@gmail.com>
2026-02-02 09:13:27 +0530

    water jug problem 2 (out put).py

---
+++ water jug problem 2 (out put).py
@@ -0,0 +1,13 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========== RESTART: C:/Users/priya/Downloads/water jug problem 2.py ===========
+Steps (3L, 5L, 8L):
+(0, 0, 8)
+(0, 5, 3)
+(3, 2, 3)
+(0, 2, 6)
+(2, 0, 6)
+(2, 5, 1)
+(3, 4, 1)
+(0, 4, 4)
